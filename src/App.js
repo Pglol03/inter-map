43a4ed665df3cd6866b5c85efeacf58e537ded79
Pradyun Gayakwad <pradyungayakwad@gmail.com>
2023-02-21 22:54:44 +0530

    FIRST COMMIT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "../node_modules/react-internet-meter/dist/index.css"
+import { ReactInternetSpeedMeter } from "react-internet-meter";
+function ConnectionSpeed() {
+  const [checkSpeed, SetCheckSpeed] = React.useState(
+    "Finding internet speed."
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="mb-3">React Find Internet Speed Example</h2>
+      <ReactInternetSpeedMeter
+        txtSubHeading="Internet connection is slow"
+        outputType="" // "alert"/"modal"/"empty"
+        customClassName={null}
+        pingInterval={2000} // milliseconds
+        txtMainHeading="Opps..."
+        thresholdUnit="megabyte" // "byte" , "kilobyte", "megabyte"
+        threshold={50}
+        imageUrl="https://www.sammobile.com/wp-content/uploads/2019/03/keyguard_default_wallpaper_silver.png"
+        downloadSize="2550420" //bytes
+        callbackFunctionOnNetworkDown={(data) =>
+          console.log(`Internet speed : ${data}`)
+        }
+        callbackFunctionOnNetworkTest={(data) => SetCheckSpeed(data)}
+      />
+      <div className="card-body mt-4">
+        <span className="display-1">{checkSpeed} MB/s</span>
+      </div>
     </div>
   );
 }
-
-export default App;
+export default ConnectionSpeed;
